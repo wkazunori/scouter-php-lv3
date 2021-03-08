b856336e76c6f9d54f5fac05dda7b8b312534ebf
wkazunori <wakamatsu.kazunori@gmail.com>
2021-03-09 08:01:29 +0900

    test

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,5 +10,7 @@ class PostsController extends Controller
 {
     public function index()
     {
+        $posts = Post::orderBy('created_at', 'desc')->get();
+        return view('posts.index', ['posts' => $posts]);
     }
 }
