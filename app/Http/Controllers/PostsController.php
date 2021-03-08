a35e9562f7f7dad530b98cce07538230673a4848
wkazunori <wakamatsu.kazunori@gmail.com>
2021-03-09 08:07:18 +0900

    失敗させる

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,7 +10,7 @@ class PostsController extends Controller
 {
     public function index()
     {
-        $posts = Post::orderBy('created_at', 'desc')->get();
-        return view('posts.index', ['posts' => $posts]);
+        // $posts = Post::orderBy('created_at', 'desc')->get();
+        // return view('posts.index', ['posts' => $posts]);
     }
 }
